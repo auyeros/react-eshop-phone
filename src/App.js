a89382ea5418a6e42be6e51d3d1ae235ef7b4375
auyeros <auyerosignacio@gmail.com>
2020-01-09 20:38:48 -0300

    formatting

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import React, { Component } from 'react';
-import {Switch,Route} from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from '../src/components/NavBar'
-import ProductList  from '../src/components/ProductList';
-import Details from '../src/components/Details';
-import Cart from '../src/components/Cart';
-import Default from '../src/components/Default';
-import Modal from './components/Modal'
-
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "../src/components/NavBar";
+import ProductList from "../src/components/ProductList";
+import Details from "../src/components/Details";
+import Cart from "../src/components/Cart";
+import Default from "../src/components/Default";
+import Modal from "./components/Modal";
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar></Navbar>
-        <Switch>
-        <Route exact path="/" component={ProductList}>
-        </Route>
-        <Route path="/details" component={Details}>
-        </Route>
-        <Route path="/cart" component={Cart}>
-        </Route>
-        <Route component={Default}>
-        </Route>
-        </Switch>
-        <Modal></Modal>
-              
-              
-      </React.Fragment>
-    )
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar></Navbar>
+                <Switch>
+                    <Route exact path="/" component={ProductList}></Route>
+                    <Route path="/details" component={Details}></Route>
+                    <Route path="/cart" component={Cart}></Route>
+                    <Route component={Default}></Route>
+                </Switch>
+                <Modal></Modal>
+            </React.Fragment>
+        );
+    }
 }
 export default App;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react'
-import Product from './Product'
-import Title from './Title';
-import {ProductConsumer} from '../context'
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
-
- 
     render() {
         return (
-            
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
-                        <Title name="our" title="products">  </Title>
-                    <div className="row">
-                    <ProductConsumer>
-                        {
-                            value=>{
-                                return value.products.map(
-                                    product =>{
-                                        return <Product key={product.id} product={product} > </Product>;
-                            }) 
-                        }}
-                    </ProductConsumer>
-                    </div>
+                        <Title name="our" title="products">
+                            {" "}
+                        </Title>
+                        <div className="row">
+                            <ProductConsumer>
+                                {value => {
+                                    return value.products.map(product => {
+                                        return (
+                                            <Product
+                                                key={product.id}
+                                                product={product}
+                                            >
+                                                {" "}
+                                            </Product>
+                                        );
+                                    });
+                                }}
+                            </ProductConsumer>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
-            )
-                    }
-                
-                };
-
-
+        );
+    }
+}
